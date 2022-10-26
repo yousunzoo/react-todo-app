b958fb2f0d15840d8f0d5364fba4224058074d91
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-10-26 10:02:11 +0900

    set ToDos with recoil

--- TODOLIST.md
+++ TODOLIST.md
@@ -73,3 +73,14 @@
 
 - 입력값이 submit 되고 검사를 통과하면 다시 input을 비우고 싶다.
   => setValue
+
+---
+
+# Recoil
+
+## useRecoilState
+
+- useRecoilState 함수는 value와 modifier 함수를 반환한다. (useState와 같은 역할)
+- useRecoilValue 와 useSetRecoilState를 합친 역할
+
+- oldToDos 자체는 배열이지만 ...oldToDos는 배열 안의 요소들을 반환한다.
---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,56 @@
+import { useForm } from "react-hook-form";
+import { atom, useRecoilState, useRecoilValue } from "recoil";
+
+interface IForm {
+  toDo: string;
+}
+
+const toDoState = atom<IToDo[]>({
+  key: "toDo",
+  default: [],
+});
+
+interface IToDo {
+  text: string;
+  id: number;
+  category: "TO_DO" | "DOING" | "DONE"; // 옵션을 제한할 수 있음
+}
+
+function ToDoList() {
+  const [toDos, setToDos] = useRecoilState(toDoState);
+
+  const { register, handleSubmit, setValue, formState } = useForm<IForm>();
+  const handleValid = ({ toDo }: IForm) => {
+    console.log("add to do", toDo);
+    setToDos((oldToDos) => [
+      { text: toDo, id: Date.now(), category: "TO_DO" },
+      ...oldToDos,
+    ]);
+    setValue("toDo", "");
+  };
+
+  console.log(toDos);
+
+  return (
+    <div>
+      <h1>To Dos</h1>
+      <hr />
+      <form onSubmit={handleSubmit(handleValid)}>
+        <input
+          {...register("toDo", {
+            required: "Please write a To Do",
+          })}
+          placeholder="Write a to do"
+        />
+        <button>Add</button>
+      </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <li key={toDo.id}>{toDo.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ToDoList;
